ee7b3edbfc690cabfa7f23852a8537ba69910212
Tim Bazuin <krageon@gmail.com>
2013-06-28 17:10:08 +0200

    added player adding to facilitate future XML addition

--- src/controller/PlayerController.java
+++ src/controller/PlayerController.java
@@ -8,7 +8,7 @@ import model.Player;
 public class PlayerController {
 	private ArrayList<Player> players;
 	public static PlayerController instance;
-
+	
 	public static PlayerController getInstance(){
 		if (instance == null)
 			instance = new PlayerController();
@@ -16,39 +16,39 @@ public class PlayerController {
 	}
 	
 	private PlayerController() {
-		
+		players = new ArrayList<Player>();
 	}
-
-	public void setGames(ArrayList<Game> games) {
-		addDefaultPlayers(games);
-	}
-
+	
 	private void addDefaultPlayers(ArrayList<Game> games) {
 		String[] names = new String[] { "Ruben", "Marcus", "Tim", "Stan",
 				"Eloy", "Stefan", "David", "Rob" };
-		players = new ArrayList<Player>();
-
-		for (int i = 0; i < names.length; i++) {
-			Player newPlayer = new Player(names[i], games);
-			players.add(newPlayer);
-		}
+		
+		addPlayers(names);
 	}
-
+	
+	private void addPlayers(String[] peopleNames) {
+		for (String name : peopleNames)
+			players.add(new Player(name, GameController.getInstance().getGames()));
+	}
+	
+	public int getCountActivePlayers() {
+		int active = 0;
+		for (Player player : players)
+			if (player.isActive())
+				active++;
+		return active;
+	}
+	
 	public int getCountPlayers() {
 		return players.size();
 	}
-
-	public int getCountActivePlayers() {
-		int active = 0;
-		for (Player player : players) {
-			if (player.isActive())
-				active++;
-		}
-		return active;
-	}
-
+	
 	public ArrayList<Player> getPlayers() {
 		return players;
 	}
-
+	
+	public void setGames(ArrayList<Game> games) {
+		addDefaultPlayers(games);
+	}
+	
 }
